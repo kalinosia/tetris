fa7e1722f45e06a8e77f8379a5bd4d192327578d
kalinosia <kalinowska953@gmail.com>
2020-06-10 15:40:36 +0200

    działa, tetris, start, score i gra ok

--- tetris.js
+++ tetris.js
@@ -1,82 +1,61 @@
 
+ document.getElementById("myButton").disabled=false;
+//document.getElementById("tetris").focus=true;//po kliknięciu żeby można było sterować
+ function start(){
+	 
+
 const cvs = document.getElementById("tetris");
-const cvs = document.getElementById("tetris");//canvas z html
 const ctx = cvs.getContext("2d"); //contex-<ctx
+//context.fillText('Start', 150, 300);	
 
-ctx.fillRect(0,0, canvas.width, canvas.height);
 
-const ROW=20;
-const COLUMN=10;
-const backgroundcolor="#222";
-
+const scoreElement = document.getElementById("score");
+//ctx.fillRect(0,0, canvas.width, canvas.height);
+const backgroundcolor="#111";
 const ROW=20;   //klasyczny tetris wymiary 
 const COLUMN=10; //pamiętając że canvas 200 x 400
-const SQ=squareSize=20;  //daje nam kwadrat 20x20
+const SQ=30;  //daje nam kwadrat 20x20
 
 //rysuj kwadrat.....................................................................................................
 function drawSquare(x,y,color){//{nr of SQ from the left,right,color) SQ=20px X 20px
 	ctx.fillStyle=color;
 	ctx.fillRect(x*SQ,y*SQ,SQ,SQ); //(ile od brzegulewego, ile od brzegu górnego, width,height)
 	
-	ctx.strokeStyle="WHITE"; //obramowanie
+	ctx.strokeStyle="#222"; //obramowanie
 	ctx.strokeRect(x*SQ,y*SQ,SQ,SQ);
 }
 
-let board=[];
+
 //tablica utwórz...........................................................................................................
 let board=[];//CREATE the board
 for (r=0;r<ROW;r++){
   board[r]=[];
   for(c=0; c<COLUMN;c++){
-    board[r][c]="white"
     board[r][c]=backgroundcolor;
   }
 }
-function drawBoard(){
-}//rysuj tablicę
+
 function drawBoard(){//draw
   for(r=0;r<ROW;r++){
-    for(c=0;c<COL;c++){
+    for(c=0;c<COLUMN;c++){
       drawSquare(c,r,board[r][c]);
- function drawBoard(){
-  }
-}
+}}}
   drawBoard();
+  
 
+//random piece function............................................................................................
+
+const PIECES = [ [z,"red"], [s,"green"], [t,"yellow"], [o,"blue"], [l,"purple"],
+ [i,"cyan"], [j,"#CC6600"]];//https://www.schemecolor.com/tetris-game-color-scheme.php  
+ 
+function randomPiece(){
+    let r = randomN = Math.floor(Math.random() * PIECES.length) // 0-6 bo tyle element.7
+    return new Piece( PIECES[r][0],PIECES[r][1]);
+}
+
+let p = randomPiece();
   
 //.............................................................................................................................
- /* 
-const z =[
-  [[1,1,0], //z=[0]
-  [0,1,1],
- const z =[
-  [0,1,1],
-  [0,1,0]],
-          
-  [[0,0,0],
-  [[0,0,0], //z[2] etc
-  [1,1,0],
-  [0,1,1]],
-  
-const z =[
-  [1,1,0],
-  [1,0,0]] 
-];
-
-let piece=z[0];
-*/
-//draw the tetrominoes ............................................................................................
-/*  let piece=z[0];     //rysowanie w macierzy 1=kolor 0 nie
-const pieceColor="orange";
-
-for(r=0;r<piece.length;r++){
-  for(c=0;c<piece.length;c++){
-    if(piece[r][c]){
- for(r=0;r<piece.length;r++){
-    }
-  }
-};
-let piece=new Piece(z,"blue");*/
 function Piece(tetromino,color){
     this.tetromino = tetromino;
     this.color = color;
@@ -111,58 +90,6 @@ Piece.prototype.unDraw = function(){    //drawSquare(this.x + c,this.y + r, this
     this.fill(backgroundcolor);						//z tego zmiana do tego co na górze tylko zmiana color
 }
 //move the activeTetromino.......................................................................................
-
-//Control the piece.....................................................................................................
-document.addEventListener("keydown", CONTROL); //Control
-
-function CONTROL(event){										//Kontrola przyciskami - srzałkami
-	if(event.keyCode==37){
-		piece.moveLeft();				//http://pomle.github.io/keycode/ best ever
-		 dropStart = Date.now(); // po każdym obrocie, ruchu musimy rozpocząć odliczanie
-	}
-	else if (event.keyCode==38){
-		piece.rotate();
-		 dropStart = Date.now();
-	}
-	else if (event.keyCode==39){
-		piece.moveRight();
-		 dropStart = Date.now();
-	}
-	else if (event.keyCode==40){
-		piece.moveDown();
-	}
-}
-
-//Collision detection Function..........................................................................................
-//przy opadaniu i dotknięciu niżej klock i  czy koniec pola
-Piece.prototype.collision = function(x,y,piece){      //KOLIZJA
-    for( r = 0; r < piece.length; r++){                
-        for(c = 0; c < piece.length; c++){
-            // if the square is empty, we skip it
-            if(!piece[r][c]){ 										//?? jeżeli nie backgroundcolor
-                continue;
-            }
-            // coordinates of the piece after movement
-            let newX = this.x + c + x;  //this.x + c + x from this.collision(x,y,piece);
-            let newY = this.y + r + y;
-            
-            // conditions new if they're out of field then collision is true
-            if(newX < 0 || newX >= COL || newY >= ROW){ 
-                return true;
-            }
-            // skip newY < 0; board[-1] will crush our game---tetromino start y<0
-            if(newY < 0){
-                continue;
-            }
-            // check if there is a locked piece alrady in place
-            if( board[newY][newX] != backgroundcolor){ //jeżeli nie kolor tła to znaczy że coś jest
-                return true;
-            }
-        }
-    }
-    return false;
-}
-//........................................................................................................................................ 
  // move Down the piece                                             MOVE
 Piece.prototype.moveDown = function(){
     if(!this.collision(0,1,this.activeTetromino)){ //jeżeli brak kolizji
@@ -176,6 +103,7 @@ Piece.prototype.moveDown = function(){
     }
     
 }
+//...........................
 																			  // W PRAWO, W LEWO
 // move Right the piece
 Piece.prototype.moveRight = function(){
@@ -194,12 +122,12 @@ Piece.prototype.moveLeft = function(){
         this.draw();
     }
 }
- 
- // rotate the piece.............................................................................................................
-Piece.prototype.rotate = function(){
-   
+
+ // rotate the piece...............................................................................
+
+Piece.prototype.rotate = function(){   
   // if (tetrmonioN==4)tetrominoN=0;
-	//else{
+	//else{																		!!!!!!!!!!!!!!!!!!!!!!!!SPOSÓB TU !!!!!!!!!
 	//let tetrominoN = this.tetromino[(this.tetrominoN + 1)];
 	//}
 	let nextPattern = this.tetromino[(this.tetrominoN + 1)%this.tetromino.length]; 
@@ -208,7 +136,7 @@ Piece.prototype.rotate = function(){
    let kick = 0; //cofnięcie jeżeli kolizja na razie 0
    
     if(this.collision(0,0,nextPattern)){
-        if(this.x > COL/2){  // it's the right wall po x większe niż połowa
+        if(this.x > COLUMN/2){  // it's the right wall po x większe niż połowa
             kick = -1; // we need to move the piece to the left
         }else{// it's the left wall
             kick = 1; // we need to move the piece to the right
@@ -222,18 +150,9 @@ Piece.prototype.rotate = function(){
         this.activeTetromino = this.tetromino[this.tetrominoN];
         this.draw();
     }
-}
 
-//random piece function............................................................................................
-const PIECES = [ [z,"red"], [s,"green"], [t,"yellow"], [o,"blue"], [l,"purple"],
- [i,"cyan"], [j,"orange"]];//https://www.schemecolor.com/tetris-game-color-scheme.php  ???
+}
  
-function randomPiece(){
-    let r = randomN = Math.floor(Math.random() * PIECES.length) // 0-6 bo tyle element.7
-    return new Piece( PIECES[r][0],PIECES[r][1]);
-}
-
-let p = randomPiece();
 //................................................................................................................................
 let score = 0;
 //........................................................................
@@ -257,17 +176,17 @@ Piece.prototype.lock = function(){                        //LOCK() FUNCTION
     // remove full rows
     for(r = 0; r < ROW; r++){ //sprawdzamy wszystkie wiersze od 0
         let isRowFull = true; //czy cały wiersz pełny?
-        for( c = 0; c < COL; c++){ //po wszystkich kolumnach patrzymy w wierszu
+        for( c = 0; c < COLUMN; c++){ //po wszystkich kolumnach patrzymy w wierszu
             isRowFull = isRowFull && (board[r][c] != backgroundcolor); //jeżeli w board r lub c jest false wszystko będzie false i  nie pełny wiersz
         }
         if(isRowFull){     //if the row is full, jeżeli pełny
             for( y = r; y > 1; y--){  // we move down all the rows above it
-                for( c = 0; c < COL; c++){
+                for( c = 0; c < COLUMN; c++){
                     board[y][c] = board[y-1][c]; //przesuamy w dół czyli w górę bo y=0 top
                 }																	//dokończy ć????????????
             }
             // the top row board[0][..] has no row above it
-            for( c = 0; c < COL; c++){
+            for( c = 0; c < COLUMN; c++){
                 board[0][c] = backgroundcolor;
             }
             // increment the score
@@ -280,22 +199,96 @@ Piece.prototype.lock = function(){                        //LOCK() FUNCTION
     // update the score
     scoreElement.innerHTML = score;
 }
+//Collision detection Function..........................................................................................
+//przy opadaniu i dotknięciu niżej klock i  czy koniec pola
+Piece.prototype.collision = function(x,y,piece){      //KOLIZJA
+    for( r = 0; r < piece.length; r++){                
+        for(c = 0; c < piece.length; c++){
+            // if the square is empty, we skip it
+            if(!piece[r][c]){ 										//?? jeżeli nie backgroundcolor
+                continue;
+            }
+            // coordinates of the piece after movement
+            let newX = this.x + c + x;  //this.x + c + x from this.collision(x,y,piece);
+            let newY = this.y + r + y;
+            
+            // conditions new if they're out of field then collision is true
+            if(newX < 0 || newX >= COLUMN || newY >= ROW){ 
+                return true;
+            }
+            // skip newY < 0; board[-1] will crush our game---tetromino start y<0
+            if(newY < 0){
+                continue;
+            }
+            // check if there is a locked piece alrady in place
+            if( board[newY][newX] != backgroundcolor){ //jeżeli nie kolor tła to znaczy że coś jest
+                return true;
+            }
+        }
+    }
+    return false;
+}
+//........................................................................................................................................ 
+//Control the piece.....................................................................................................
+document.addEventListener("keydown", CONTROL); //Control
+
+function CONTROL(event){			
+							//Kontrola przyciskami - srzałkami
+	
+	if(event.keyCode==37){
+		lastKey=37;
+		p.moveLeft();				//http://pomle.github.io/keycode/ best ever
+		 //dropStart = Date.now(); // po każdym obrocie, ruchu musimy rozpocząć odliczanie
+	}
+	else if (event.keyCode==38){
+		p.rotate();
+		// dropStart = Date.now();      //ale bez sensu bo da się stać w miejscu długo
+	}
+	else if (event.keyCode==39){
+		p.moveRight();
+		// dropStart = Date.now();
+	}
+	else if (event.keyCode==40){
+		p.moveDown();p.moveDown();//dwa razy zapewnia szybsze spadanie
+	}
+}
+
+
+
+
 //.....................................................................................................................................
 // drop the piece every 1sec 								!!!!!!!!!	CZAS !!!!!!!!!!!
+let czasPoziomu=1500;
+
 
 let dropStart = Date.now();
 let gameOver = false;
 function drop(){
     let now = Date.now();
     let delta = now - dropStart;
-    if(delta > 1000){
+    if(delta > czasPoziomu){
         p.moveDown();
         dropStart = Date.now();
     }
     if( !gameOver){
         requestAnimationFrame(drop);
     }
+
+	
+	if(score==100)czasPoziomu=1100;
+else if(score==200)czasPoziomu=1000;
+else if(score==300)czasPoziomu=900;
+else if(score==400)czasPoziomu=800;
+else if(score==500)czasPoziomu=700;
+else if(score==600)czasPoziomu=600;
+else if(score==700)czasPoziomu=500;
+else if(score==800)czasPoziomu=400;
+else if(score==900)czasPoziomu=300;
+else if(score==1000)czasPoziomu=200;
+else czasPoziomu=czasPoziomu;
 }
 
 drop();
 //...............................................................................................................................
+
+}
